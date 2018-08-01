8e52f84b268e98220363d6ece25e616b5f9a67a8
Brian Hudson <hudson_ops@brians-imac.attlocal.net>
2018-08-01 12:06:50 -0400

    Functional test now checks we can input a to-do item

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,17 +18,33 @@ class NewVisitorTest(unittest.TestCase):
 
         # He notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
 # He is prompted on the homepage to enter the first item on his to-do list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
 # He types "Solve a Codewars kata"
+        inputbox.send_keys('Solve a Codewars kata')
 
 # When he hits enter, the page updates, and now the page lists
 # "1: Solve a Codwewars kata" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Solve a Codewars kata' for row in rows)
+        )
 
 # There is still a text box inviting him to add another item. He enters
 # "Mow and weedeat the lawn"
+        self.fail('Finish the test!')
 
 # The page updates again, and now shows both items on his list
 
