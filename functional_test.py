5fe599a589279ece98c7fa4b624073a19598827c
Brian Hudson <hudson_ops@brians-imac.attlocal.net>
2018-08-04 16:12:02 -0400

    Added cheat to get FT to pass when chechecking that new list item is added.

--- functional_test.py
+++ functional_test.py
@@ -38,10 +38,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Solve a Codewars kata' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Solve a Codewars kata', [row.text for row in rows])
+
 
 # There is still a text box inviting him to add another item. He enters
 # "Mow and weedeat the lawn"
