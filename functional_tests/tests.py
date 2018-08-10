a93603129dc3a00cb10c64afbdbefb22fae7d839
Brian Hudson <hudson_ops@yahoo.com>
2018-08-09 17:21:21 -0700

    New URL, view and template to display lists, also corrected typo in functional_tests that was causing test to fail

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -87,8 +87,32 @@ class NewVisitorTest(LiveServerTestCase):
 
 ##We use a new browser session to make sure that no information
 ##of Philemon's is coming through from cookies etc
-        sle.fbrowser.quit()
-        self.browser = webdriver.Firefox()
+        self.browser.quit()
+        self.browser = webdriver.Firefox(executable_path="/usr/local/bin/geckodriver")
+
+        #Francis visit the home page. There is no sign of Philemon's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Solve a Codewars kata', page_text)
+        self.assertNotIn('Mow and weedeat the lawn', page_text)
+
+        #Franceis starts a new list by entering a new item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        #Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, philemon_list_url)
+
+        #Again, there is no trace of Philemon's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Sove a Codewars kata', page_text)
+        self.AssertIn('Buy milk', page_text)
+
+        #Satisfied, they both go back to sleep
 
 
 
